3988be2aa3c5b5ca7cfca89856ab41d649a49d5a
Hayk Grigoryan <hayk.grigoryan.developer@gmail.com>
2017-03-05 14:57:32 +0400

    Changed wp-config db user

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'menu_blog');
+define('DB_NAME', 'wordpress');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpressuser');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'G@9wnb4RT,K5[(j"');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
